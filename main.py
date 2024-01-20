4c1d31b514bc9277d6b56fda27c737839eadef68
kapowaaaa <theqwantyt@gmail.com>
2024-01-20 09:39:03 +0300

    game field

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print('hi')
-print('hi')
-print('hi')
-print('hi')
+import random
+
+pole=[[0] * 5 for _ in range(5)]
+for i in range(5):
+    for j in range(5):
+        pole[i][j] = random.randint(1, 3)
+for row in pole:
+    print(row)
