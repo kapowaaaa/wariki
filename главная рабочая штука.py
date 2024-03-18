1ad81f18da1eeb68fc31655225815ef808e2ae8a
kapowaaaa <theqwantyt@gmail.com>
2024-03-19 00:51:32 +0300

    нея

--- главная рабочая штука.py
+++ главная рабочая штука.py
@@ -5,54 +5,64 @@ from PyQt5.QtCore import Qt
 import random
 import math
 
-total_rows = 5
-column = 15
-balls_field = []
-x = 0
-y = 0
+screen_width, screen_height = 0, 0
 
-for i in range(total_rows):
-    if i % 2 == 0:
-        x = 0
-    else:
-        x = 60
-    row_list = []
-    for j in range(column):
-        color = random.choice([(255, 0, 0), (0, 255, 0), (0, 0, 255)])  # Randomly choose red, green, or blue
-        cell = [x, y, color]
-        x += 120
-        row_list.append(cell)
-    balls_field.append(row_list)
-    y += 103
 
 class GameWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.showMaximized()
+
+        total_rows = 5
+        column = 16
+        balls_field = []
+
+        x = 0
+        y = 0
+
+        # screen_width = self.size().width()
+        # screen_height = self.size().height()
+        print(screen_width, screen_height)
+
+        self.ball_size = min(screen_width // column, screen_height // total_rows)
+
+        for i in range(total_rows):
+            if i % 2 == 0:
+                x = 0
+            else:
+                x = self.ball_size // 2
+            row_list = []
+            for j in range(column):
+                color = random.choice([(255, 0, 0), (0, 255, 0), (0, 0, 255)])  # Randomly choose red, green, or blue
+                cell = [x, y, color]
+                x += self.ball_size
+                row_list.append(cell)
+            balls_field.append(row_list)
+            y += self.ball_size
+
         self.row = total_rows
         self.shooter_angle = 180
 
         self.setWindowTitle("Bubble Shooter Game")
-        self.setGeometry(0, 0, 1920, 1080)
+        self.setGeometry(0, 0, screen_width, screen_height)
 
         self.game_field = QLabel(self)
-        # self.game_field.setGeometry(0, 0, column*175, self.row*150)
-        # self.game_field.setStyleSheet("background-color: white; border: 1px solid black;") #сцена
-        self.game_field.setGeometry(0, 0, column * 175, self.row * 150)
-        # self.game_field.setGeometry(0, 0, 1920, 1080)
-        # self.game_field.setStyleSheet("background-color: white; border: 1px solid black;") какаято ересь ненужная
+        self.game_field.setGeometry(0, 0, column * self.ball_size, self.row * self.ball_size)
 
         for row in balls_field:
             for cell in row:
                 x, y, color = cell
                 ball_label = QLabel(self.game_field)
-                ball_label.setGeometry(x, y, 120, 120)
+                ball_label.setGeometry(x, y, self.ball_size, self.ball_size)
                 ball_label.setStyleSheet(
-                    f"background-color: rgb({color[0]}, {color[1]}, {color[2]}); border-radius: {120 // 2}px; border: 1px solid black;")
+                    f"background-color: rgb({color[0]}, {color[1]}, {color[2]}); border-radius: {self.ball_size // 2}px; border: 1px solid black;")
                 ball_label.color = color
 
         self.shooter = QLabel(self)
-        self.shooter.setGeometry(960 - 120 // 2, 930 - 120 // 2, 100, 100)
+        self.shooter.setGeometry(screen_width // 2 - self.ball_size // 2, screen_height - self.ball_size // 2,
+                                 self.ball_size // 2,
+                                 self.ball_size // 2)
         self.shooter.setStyleSheet("background-color: gray; border-radius: 45px; border: 1px solid black;")
 
         self.setMouseTracking(True)  # Enable mouse tracking
@@ -63,14 +73,14 @@ class GameWindow(QMainWindow):
         painter.setBrush(QBrush(Qt.black))
         painter.save()
         painter.translate(self.width() / 2,
-                          self.height() - 30)
+                          self.height() - self.ball_size // 2)
         painter.rotate(self.shooter_angle)
-        painter.drawRect(-15, -60, 30, 60)
+        painter.drawRect(-10, -self.ball_size // 2, 30, self.ball_size)
         painter.restore()
 
     def mouseMoveEvent(self, event):
-        dx = event.x() - 960
-        dy = event.y() - 930
+        dx = event.x() - screen_width // 2
+        dy = event.y() - screen_height
         angle = math.degrees(math.atan2(dy, dx))
         self.shooter_angle = angle
         self.update()
@@ -79,39 +89,46 @@ class GameWindow(QMainWindow):
         if event.button() == Qt.LeftButton:
             color = random.choice([(255, 0, 0), (0, 255, 0), (0, 0, 255)])
             ball_label = QLabel(self.game_field)
-            ball_label.setGeometry(960 - 120 // 2, 930 - 120 // 2, 120, 120)
+            ball_label.setGeometry(screen_width // 2 - self.ball_size // 2,
+                                   screen_height - self.ball_size // 2, self.ball_size,
+                                   self.ball_size)
             ball_label.setStyleSheet(
-                f"background-color: rgb({color[0]}, {color[1]}, {color[2]}); border-radius: {120 // 2}px; border: 1px solid black;")
+                f"background-color: rgb({color[0]}, {color[1]}, {color[2]}); border-radius: {self.ball_size // 2}px; border: 1px solid black;")
             ball_label.color = color
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
             self.close()
 
+
 class StartWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        global screen_width, screen_height
+
+        screen_width = self.size().width()
+        screen_height = self.size().height()
+
         self.setWindowTitle("Bubble Shooter Game")
-        self.setGeometry(0, 0, 1920, 1080)
+        self.setGeometry(0, 0, screen_width, screen_height)
 
         start_button = QPushButton("Начать игру", self)
-        start_button.setGeometry(960, 540, 100, 50)
+        start_button.setGeometry(screen_width // 2 - 50, screen_height // 2 - 25, 100, 50)
         start_button.clicked.connect(self.start_game)
 
         exit_button = QPushButton("Выйти", self)
         exit_button.setGeometry(0, 0, 100, 50)
         exit_button.clicked.connect(self.close)
 
-
     def start_game(self):
         self.close()
         self.game_window = GameWindow()
-        self.game_window.show()
+        # self.game_window.showMaximized()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     start_window = StartWindow()
-    start_window.show()
+    start_window.showMaximized()
     sys.exit(app.exec_())
-
